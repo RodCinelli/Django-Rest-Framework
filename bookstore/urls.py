0b6ca0a8c883a3347a6b13280cb87707f92e4087
Rodrigo Cinelli <rodcinelli@gmail.com>
2025-02-05 00:32:34 -0300

    Alterando as urls para homepage

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -18,12 +18,15 @@ Including another URLconf
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.shortcuts import redirect
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    # Redireciona a homepage para /bookstore/v1/product/
+    path("", lambda request: redirect("/bookstore/v1/product/"), name="homepage"),
     path("__debug__/", include("debug_toolbar.urls")),
     path("admin/", admin.site.urls),
-    re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
-    re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    re_path(r'^bookstore/(?P<version>(v1|v2))/', include('order.urls')),
+    re_path(r'^bookstore/(?P<version>(v1|v2))/', include('product.urls')),
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
 ]
